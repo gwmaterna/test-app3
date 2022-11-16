0e09e7055801e6fa6f74ca1a1e82f1004f0c46a6
gwmaterna <110205850+gwmaterna@users.noreply.github.com>
2022-11-16 13:14:20 -0800

    second commit

--- src/components/news-feed.js
+++ src/components/news-feed.js
@@ -5,13 +5,31 @@ let e = React.createElement;
 
 export default class NewsFeed extends React.Component {
     render() {
+        let comments = [
+        {
+            content: 'This is my comment',
+            username: 'Tommy',
+            date: '10-30-2022'
+        },
+        {
+            content: 'This is another comment',
+            username: 'Sammy',
+            date: '10-25-2022'
+        },
+        {
+            content: 'Here\s a comment from me',
+            username: 'Greg',
+            date: '10-01-2022'
+        }
+    ];
         return (
             <div className="container">
+                <Post {...{comments: comments, content: 'This is my post content!'}}/>
                 <Post />
                 <Post />
             </div>
         );
-
+              
     // Below was replace w/ lines 8-13 using JSX    
     // return e('div',
     //     {class:'container'},
--- src/components/post.js
+++ src/components/post.js
@@ -6,21 +6,65 @@ import Comment from './comment';
 let e = React.createElement;
 
 export default class Post extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: props.comments,
+            content: props.content
+            // count: 0
+        };
+    }
     render() {
+
+        // setTimeout(() => this.setState(x => ({count: x.count + 1}), () => console.log
+        // (this.state.count)), 2000);
+
+        // Replaced 3 lines below w/ the above
+        // let tempComments = this.state.comments;
+        // setTimeout(() => this.setState({content: 'state updated', comments: []},
+        // () => setTimeout(() => this.setState({comments: tempComments}), 2000)), 2000);
+
+       
+        // let commentOne = {
+        //     content: 'This is my post',
+        //     username: 'Tommy',
+        //     date: '10-30-2022'
+        // };
+        // let commentTwo = {
+        //     content: 'This is another post',
+        //     username: 'Sammy',
+        //     date: '10-25-2022'
+        // };
+        // let commentThree = {
+        //     content: 'Here\s a post from me',
+        //     username: 'Greg',
+        //     date: '10-01-2022'
+        // };
+
+        // let comments = [];
+
+        let comments;
+
+        if (this.state.comments) {
+            comments = this.state.comments.map((comment, index) => 
+            <Comment key = {index} {...comment} />);
+
+            // for (let comment of this.state.comments) {
+            //     comments.push(<Comment {...comment} />);
+            // }
+        }
         return (
             <div className="card w-75">
                 <div className="card-header bg-primary text-white">
                     Username and Time
                 </div>
                 <div className="card-body">
-                    This is where our content goes
+                    {this.state.content}
                 </div>
                 <div className="card-footer">
                     <LikeButton />
                     <ReplyButton />
-                    <Comment />
-                    <Comment />
-                    <Comment />
+                    {comments}
                 </div>
             </div>
         );
@@ -44,5 +88,6 @@ export default class Post extends React.Component {
     //         e(Comment, {}, null)
     //         )
     //     );
+    
     }
 }
